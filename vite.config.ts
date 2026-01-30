cea9a62a86fc2dfa6ed48b347ad2c8cecdd3ea38
benjiiminhuang <dongyapistonring01@gmail.com>
2026-01-30 09:01:19 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Gphone/',  // 添加这行让 GitHub Pages 能正确找到资源文件
       server: {
         port: 3000,
         host: '0.0.0.0',

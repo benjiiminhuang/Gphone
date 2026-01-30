6a9f8f82b85ddb4a66621f1636c7f1d93987e106
benjiiminhuang <dongyapistonring01@gmail.com>
2026-01-30 09:22:11 +0800

    Update Hero.tsx

--- Hero.tsx
+++ Hero.tsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import { ChevronRight } from 'lucide-react';
-import { Product } from '../types';
+import { Product } from './types';
 
 interface HeroProps {
   product: Product;
